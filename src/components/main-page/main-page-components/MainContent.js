a5cb1eb273ba0883740aa90d66b08a62ea92953d
Mariyan Nikolov <71733160+Marian9364@users.noreply.github.com>
2022-07-26 18:03:46 +0300

    MainPage background image import
    Background of the main page is being imported from URL instead of local file.

--- src/components/main-page/main-page-components/MainContent.js
+++ src/components/main-page/main-page-components/MainContent.js
@@ -1,7 +1,10 @@
 export function MainContent() {
-    return (
-        <div className="imgWrapper">
-        <img src="../../img/grandmothersPic.jpeg" alt="Bulgarian grandmothers near Gabrovo, Bulgaria" width="500" height="600" />
+  return (
+    <div className="imgWrapper">
+      <img
+        src="https://img.itinari.com/page/content/original/f5d6d13a-17df-4020-a648-5455d4ca995a-istock-564566138.jpg?ch=DPR&dpr=2&w=994&s=61e674508534d8bb3699d06d56556029"
+        alt="Bulgarian grandmothers near Gabrovo, Bulgaria"
+      />
     </div>
-    )
+  );
 }
