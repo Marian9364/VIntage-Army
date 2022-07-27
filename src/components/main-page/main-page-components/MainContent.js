e940a401d57056dbc686e83bac1080bf15334811
Mariyan Nikolov <71733160+Marian9364@users.noreply.github.com>
2022-07-27 20:20:32 +0300

    background adjustments
    background image adjustments

--- src/components/main-page/main-page-components/MainContent.js
+++ src/components/main-page/main-page-components/MainContent.js
@@ -1,10 +1,9 @@
+import styles from "./MainContent.module.css";
+
 export function MainContent() {
   return (
-    <div className="imgWrapper">
-      <img
-        src="https://img.itinari.com/page/content/original/f5d6d13a-17df-4020-a648-5455d4ca995a-istock-564566138.jpg?ch=DPR&dpr=2&w=994&s=61e674508534d8bb3699d06d56556029"
-        alt="Bulgarian grandmothers near Gabrovo, Bulgaria"
-      />
+    <div className={styles.MainContentWrapper}>
+        <p className={styles.firstDescription}>Buy your unique outfit directly from the producer</p>
     </div>
   );
 }
