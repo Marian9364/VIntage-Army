c77405124080bc79f1bb128b6bbb0eb9c4ac4ae6
Mariyan Nikolov <71733160+Marian9364@users.noreply.github.com>
2022-07-28 19:55:23 +0300

    register page configurations
    register page form added

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -4,7 +4,7 @@ import styles from "./Header.module.css";
 import "../../reset.css"
 
 export const Header = () => {
-    return (<navbar>
+    return (<nav>
     <ul className={styles.header} role="list">
         <li className={styles.headerLinks}><Link to="/" className={`${styles.headerLinkElement} ${styles.home}`}>Home</Link></li>
         <li className={styles.headerLinks}><Link to="/login" className={styles.headerLinkElement}>Login</Link></li>
@@ -12,6 +12,6 @@ export const Header = () => {
         <li className={styles.headerLinks}><Link to="/about" className={styles.headerLinkElement}>About Us</Link></li>
         <li className={styles.headerLinks}><Link to="/contact" className={styles.headerLinkElement}>Contact Us</Link></li>
     </ul>
-    </navbar>)
+    </nav>)
     
 }
--- src/components/register-page/RegisterPage.js
+++ src/components/register-page/RegisterPage.js
@@ -1,10 +1,64 @@
 
+import { useState, useRef, useEffect } from "react";
 import styles from "./RegisterPage.module.css";
 
 export function RegisterPage() {
+  const [values, setValues] = useState({
+    email: '',
+    password: '',
+    rePass: '',
+  });
+
+  
+  
+  const changeHandler = (e) => {
+    setValues(state => ({
+      ...state,
+      [e.target.name]: e.target.value}));
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log(values);
+  }
   return (
     <div className={styles.mainWrapper}>
         <h1>Register Page</h1>
+        
+        <form onSubmit={submitHandler}>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input
+          id="email"
+          type="email" 
+          name="email"
+          value={values.email} 
+          placeholder="IvanIvanov@mail.com"
+          onChange={changeHandler}
+           />
+          </div>
+          <div>
+            <label htmlFor="password">Password:</label>
+            <input 
+            id="password" 
+            type="password" 
+            name="password" 
+            value={values.password} 
+            onChange={changeHandler} />
+          </div>
+          <div>
+            <label htmlFor="rePass">Repeat Password:</label>
+            <input 
+            id="rePass" 
+            type="password" 
+            name="rePass" 
+            value={values.rePass} 
+            onChange={changeHandler} />
+          </div>
+          <div>
+            <button type="submit" onSubmit={changeHandler}>Register</button>
+          </div>
+        </form>
     </div>
   );
 }
