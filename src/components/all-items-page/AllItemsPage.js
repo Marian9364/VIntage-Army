e56a5865a9a3b21c091bb846e25c3cf22ece5af3
Mariyan Nikolov <71733160+Marian9364@users.noreply.github.com>
2022-08-10 16:15:53 +0300

    single item template added
    single item template added but still not done

--- src/components/all-items-page/AllItemsPage.js
+++ src/components/all-items-page/AllItemsPage.js
@@ -1,9 +1,15 @@
 import styles from "./AllItemsPage.module.css";
+import { SingleItem } from "../single-item/SingleItem";
 
-export const AllItemsPage = () => {
+export const AllItemsPage = ({ items }) => {
   return (
     <div className={styles.mainWrapper}>
       <h1>All Items</h1>
+      {items.length > 0 ? (
+        items.map((x) => <SingleItem key={x._id} item={x} />)
+      ) : (
+        <h3 className={styles.mainWrapper}>No articles yet</h3>
+      )}
     </div>
   );
 };
--- src/components/single-item/SingleItem.js
+++ src/components/single-item/SingleItem.js
@@ -1,8 +1,16 @@
+import styles from "./SingleItem.module.css"
 
 export const SingleItem = () => {
-    return (
-        <div className={style.mainWrapper}>
-            
-        </div>
-    )
-}
+  return (
+    <div className={styles.mainWrapper}>
+      <div className={styles.innerWrapper}>
+        <h1 className={styles.heading}>Item Name</h1>
+        <h2>Here we show the Img</h2>
+        <h4>Cloth Type:</h4>
+        <h4>Size:</h4>
+        <h4>Gender:</h4>
+        <h4>price:</h4>
+      </div>
+    </div>
+  );
+};
