b2b0ab5c708ab743813e02761bae631b2e4aa2a4
Mariyan Nikolov <71733160+Marian9364@users.noreply.github.com>
2023-04-20 12:08:32 +0200

    nickname greeting from header
    Greeting only by nickname, instead of using the entire email in the header, for logged in user.

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -5,11 +5,18 @@ import { useAuthContext } from "../../contexts/AuthContext";
 
 export const Header = () => {
     const { user } = useAuthContext();
+    let emailNickname;
+
+    if (user.email) {
+        emailNickname = user.email.split('@')[0]
+    } else {
+        emailNickname = undefined;
+    }
 
     const userNav = (
         <nav>
         <ul className={styles.header} role="list">
-            <li className={styles.headerLinks}><span className={styles.headerLinkElement}><em>Welcome {user?.email}</em></span></li>
+            <li className={styles.headerLinks}><span className={styles.headerLinkElement}><em>Welcome {emailNickname}</em></span></li>
             <li className={styles.headerLinks}><Link to="/" className={styles.headerLinkElement}>Home</Link></li>
             <li className={styles.headerLinks}><Link to="/about" className={styles.headerLinkElement}>About Us</Link></li>
             <li className={styles.headerLinks}><Link to="/contact" className={styles.headerLinkElement}>Contact Us</Link></li>
