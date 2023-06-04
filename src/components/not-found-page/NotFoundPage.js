8120b6a0942b54dca6cad9b0639847b89d20f310
Mariyan Nikolov <71733160+Marian9364@users.noreply.github.com>
2023-06-04 20:03:13 +0200

    link added to not-found-page
    Link added to NotFoundPage leading to Home-Page

--- src/components/not-found-page/NotFoundPage.js
+++ src/components/not-found-page/NotFoundPage.js
@@ -1,9 +1,11 @@
+import { Link } from "react-router-dom";
 import styles from "./NotFoundPage.module.css";
 
 export const NotFoundPage = () => {
     return (
         <div className={styles.mainWrapper}>
             <h1>Page Not Found</h1>
+            <h2>Return to<Link className={styles.link} to="/">Vintage Army</Link></h2>
         </div>
     )
 }
