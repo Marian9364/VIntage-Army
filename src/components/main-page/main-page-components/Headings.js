0d02b5b05336770bfee32240752e8ed895b26af3
Mariyan Nikolov <71733160+Marian9364@users.noreply.github.com>
2023-06-04 18:51:19 +0200

    rebrand
    name change to Vintage Army

--- src/components/main-page/main-page-components/Headings.js
+++ src/components/main-page/main-page-components/Headings.js
@@ -3,7 +3,7 @@ import styles from "./Headings.module.css";
 export function Headings() {
     return (
       <div className="headings">
-          <h1 className={styles.heading}>BG Baba</h1>
+          <h1 className={styles.heading}>Vintage Army</h1>
           <h2 className={styles.subheading}>For your new authentic outlook</h2>
       </div>
     );
